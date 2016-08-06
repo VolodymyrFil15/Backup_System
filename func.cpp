e7545c9b7f196b0ddbfe5a9f97a208ccbd2ac21c
VolodymyrFil15 <volodymyr.fil15@gmail.com>
2016-08-06 15:55:41 +0300

    Deleted func.cpp

--- func.cpp
+++
@@ -1,8 +0,0 @@
-void contentDifference(QDir &sDir, QDir &dDir, QFileInfoList &diffList, int dirNum);							finished
-void on_syncBtn_clicked();																						
-void recursiveContentList(QDir &dir, QFileInfoList &contentList, int dirNum);									finished
-bool extToCopy(QFileInfo info, QStringList arr);																finished
-void setExtArray(QLineEdit *inputline, QStringList *arr);														finished
-bool check(QFileInfo info, int folder);																			finished
-int countSize(QLineEdit* inputLine, QButtonGroup* buttons);														finished
-void another
